104a21fb7c64ec6de7aee6dc42495b4f07275568
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-04-01 15:38:51 +0530

    Update Tra_Lrng.py

--- Tra_Lrng.py
+++ Tra_Lrng.py
@@ -1,120 +1,45 @@
-import numpy as np
-import torch,torchvision
-import torch.nn as nn
-import torchvision.transforms as transforms
-from torchsummary import summary
-from torch import optim
-import cv2
-import os
+import streamlit as st
 from PIL import Image
-import pandas as pd
-import seaborn as sns
-import torch.nn.functional as F
+import torch
+import torchvision.transforms as transforms
+import cv2
 
+# Load the trained model
+model = torch.load("path/to/your/model.pt")
 
-
-#pre processing
-transform=transforms.Compose([
+# Define the image transformation pipeline
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Resize((224,224)),
-     transforms.RandomAffine(degrees=(25)),
-     transforms.RandomRotation(25),
-     transforms.RandomHorizontalFlip(0.5),
-     transforms.RandomVerticalFlip(0.5),
-     transforms.Normalize((0.5,0.5,0.5),(1,1,1))
+    transforms.Normalize((0.5,0.5,0.5),(1,1,1))
 ])
 
-
-batch_size=64
-epochs=100
-num_classes=16
-
-
-
-
-# ## model creation
-
-# In[12]:
-
-
-model=torchvision.models.resnet18(pretrained=True)
-num_ftrs=model.fc.in_features
-model.fc=nn.Linear(num_ftrs,16)
-
-model=model.to(device)
-model
-
-
-# In[13]:
-
-
-summary(model,input_size=(3,224,224))
-
-
-# In[14]:
-
-
-loss_fn=nn.CrossEntropyLoss()
-optimizer=optim.SGD(model.parameters(),lr=0.01)
-
-
-# In[15]:
-
-
-path="C:\\Users\\NOUFAL\\OneDrive\\Documents\\PROJECT\\resnet_18_epochs"
-
-
-# In[16]:
-
-
-model_path="C:\\Users\\NOUFAL\\OneDrive\\Documents\\PROJECT\\resnet_18_epochs\\epoch-61.pt"
-model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
-
-
-# #### training
-
-# In[ ]:
-
-
-    
-
-
-# In[17]:
-
-
-model.eval()
-
-
-# In[19]:
-
-
-
-
-
-
-def pred(img_path,transform):
-    image=Image.open(img_path)
-    image_tensor=transform(image).float()
-    image_tensor=image_tensor.unsqueeze(0)
-    image_tensor=image_tensor.to(device)
-    output=model(image_tensor)
-    index=torch.argmax(output)
-    pred=test_data.classes[index]
-    plt.imshow(cv2.imread(img_path))
-    probs, _ = torch.max(F.softmax(output,dim=1),1)
-    print(probs)
-    if probs <0.93:
-        print("not defined")
-    else:    
+# Define the prediction function
+def predict(image):
+    image_tensor = transform(image).float()
+    image_tensor = image_tensor.unsqueeze(0)
+    output = model(image_tensor)
+    index = torch.argmax(output)
+    pred = train_data.classes[index]
+    probs, _ = torch.max(torch.softmax(output,dim=1),1)
+    if probs < 0.93:
+        return "not defined"
+    else:
         return pred
-test="C:\\Users\\NOUFAL\\Downloads\\th (2).jpg"
-print("The predicted class is :")
-pred(test,transform)
 
+# Define the Streamlit app
+def app():
+    st.set_page_config(page_title="Image Classification App", page_icon=":smiley:", layout="wide")
+    st.title("Image Classification App")
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        pred = predict(image)
+        st.write("Prediction: ", pred)
 
-
-
-# In[ ]:
+if __name__ == '__main__':
+    app()
 
 
 

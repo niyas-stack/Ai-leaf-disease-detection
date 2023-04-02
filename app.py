b378bab9ab89d67c2b4d2fdd6dea34ae2e9fd08d
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-04-02 13:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,78 @@
-import streamlit as st
+import os
 import numpy as np
-from PIL import Image
-import torch.nn.functional as F
+
 import torch
+import PIL
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+from Plant_Disease_Detection import model, train_data
+from PIL import Image
+import streamlit as st
+from werkzeug.utils import secure_filename
 
-# Define the device
+#checking for the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
-# Define the transforms for the input image
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Resize((224,224)),
-    transforms.Normalize((0.5,0.5,0.5),(1,1,1))
-])
-
-# Load the model
-model = torchvision.models.resnet18(pretrained=True)
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 16)
-model.to(device)
-model_path = "C:\\Users\\NOUFAL\\OneDrive\\Documents\\PROJECT\\resnet_18_epochs\\epoch-61.pt"
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+model_path = "C:\\Users\\user\\Documents\\project\\resnet_epoch\\epoch-81.pt"
+model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
 model.eval()
 
-# Define a function to make predictions
-def predict(image):
-    # Load and transform the input image
+classes = dict({0:'The above leaf is Cassava (Cassava Mosaic) ', 
+                1:'The above leaf is Cassava CB (Cassava Bacterial Blight)', 
+                2:'The above leaf is Cassava Healthy leaf', 
+                3:'The above leaf is Tomato Bacterial spot', 
+                4:'The above leaf is Tomato early blight',
+                5:'The above leaf is Tomato Late blight',
+                6:'The above leaf is Tomato Leaf Mold', 
+                7:'The above leaf is Tomato Septoria leaf spot',
+                8:'The above leaf is Tomato Spider mites Two-spotted spider mite', 
+                9:'The above leaf is Tomato Target Spot',
+                10:'The above leaf is Tomato Yellow Leaf Curl Virus', 
+                11:'The above leaf is Tomato mosaic virus', 
+                12:' The above leaf is Tomato healthy', 
+                13:'The above leaf is bean angular leaf spot',
+                14:'The above leaf is bean healthy', 
+                15:'The above leaf is bean rust'})
+
+#image transformation
+transform=transforms.Compose([
+transforms.ToTensor(),
+    transforms.Resize((224,224)),
+    transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),
+    transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+    ])
+
+def model_predict(img_path, model_func, transform):
+    image = Image.open(img_path)
     image_tensor = transform(image).float()
     image_tensor = image_tensor.unsqueeze(0)
     image_tensor = image_tensor.to(device)
-
-    # Make the prediction
-    output = model(image_tensor)
+    output = model_func(image_tensor)
     index = torch.argmax(output)
+    print(index)
+    pred = classes[index.item()]
     probs, _ = torch.max(F.softmax(output, dim=1), 1)
-    if probs < 0.93:
-        return "Not defined"
-    else:
-        return classes[index]
+    print(probs)
+    return pred
 
-# Define the Streamlit app
-def app():
-    st.title("ResNet-18 Image Classifier")
-    st.write("This app uses a ResNet-18 model to classify images into 16 different classes.")
-    uploaded_file = st.file_uploader("Choose an image to classify", type=["jpg", "jpeg", "png"])
-    if uploaded_file is not None:
-        # Load and display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+def main():
+    st.set_page_config(page_title="Plant Disease Detection App")
+    st.title("Plant Disease Detection App")
+    st.write("Upload an image of a plant leaf and the app will predict whether it has a disease or not.")
+    file = st.file_uploader("Upload image", type=["jpg", "jpeg", "png"])
+    if file is not None:
+        image = Image.open(file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        pred_button = st.button("Predict")
+        if pred_button:
+            with st.spinner('Predicting...'):
+                preds = model_predict(img_path=file, model_func=model, transform=transform)
+            st.success(preds)
 
-        # Make a prediction and display the result
-        pred = predict(image)
-        st.write("Prediction: ", pred)
-
-# Run the app
 if __name__ == '__main__':
-    app()
-
-
+    main()

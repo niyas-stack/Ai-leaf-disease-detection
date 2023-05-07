56ab79540dc5cb835a7997d9f21fc0fdd8d4cc51
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-07 19:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ classes = {
 }
 num_ftrs = model.fc.in_features
 model.fc = torch.nn.Linear(num_ftrs, len(classes))
-model_path = "epoch-81.pt"
+model_path = "epoch-90.pt"
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 summary(model,input_size=(3,224,224))
 model.eval()
@@ -66,12 +66,12 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', width=300)
         st.write("")
-        st.write("Classifying...")
-        pred, probs = model_predict(image, model, transform)
-        st.write(f"Prediction: {pred}")
-        st.write(f"Probability: {probs.item()}")
+        if st.button("Classify", key="classify_btn"):
+            pred, probs = model_predict(image, model, transform)
+            st.write(f"Prediction: {pred}")
+            st.write(f"Probability: {probs.item()}")
 
 if __name__ == "__main__":
     main()

cd96a904e3d3ef1aaabb791d797ac5c95b967a9f
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 05:48:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,15 +61,12 @@ transforms.ToTensor(),
     transforms.RandomVerticalFlip(),
     transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
     ])
-def model_predict(img_path,model_func,transform):
-    image=Image.open(img_path)
-    image_tensor=transform(image).float()
-    image_tensor=image_tensor.unsqueeze(0)
-    image_tensor=image_tensor.to(device)
-    output=model_func(image_tensor)
-    index=torch.argmax(output)
-    print(index)
-    pred=classes[index.item()]
+def model_predict(image, model_func, transform):
+    image_tensor = transform(image).float()
+    image_tensor = image_tensor.unsqueeze(0)
+    output = model_func(image_tensor)
+    index = torch.argmax(output)
+    pred = classes[index.item()]
     probs, _ = torch.max(F.softmax(output, dim=1), 1)
     if probs < 0.93:
         return "This is not trained yet ", probs

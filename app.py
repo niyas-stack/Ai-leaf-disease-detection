7d224fe70e7c80748e5d505c56873b44b6c240c1
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 14:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,24 +13,23 @@ import base64
 
 # Load the model
 model = torchvision.models.resnet18(pretrained=True)
-classes = {
-   0: 'The above leaf is Cassava (Cassava Mosaic)',
-    1: 'The above leaf is Cassava CB (Cassava Bacterial Blight)',
-    2: 'The above leaf is Cassava Healthy leaf',
-    3: 'The above leaf is Tomato Bacterial spot',
-    4: 'The above leaf is Tomato early blight',
-    5: 'The above leaf is Tomato Late blight',
-    6: 'The above leaf is Tomato Leaf Mold',
-    7: 'The above leaf is Tomato Septoria leaf spot',
-    8: 'The above leaf is Tomato Spider mites Two-spotted spider mite',
-    9: 'The above leaf is Tomato Target Spot',
-    10: 'The above leaf is Tomato Yellow Leaf Curl Virus',
-    11: 'The above leaf is Tomato mosaic virus',
-    12: 'The above leaf is Tomato healthy',
-    13: 'The above leaf is bean angular leaf spot',
-    14: 'The above leaf is bean healthy',
-    15: 'The above leaf is bean rust'
-}
+classes = dict({0:'The above leaf is Cassava (Cassava Mosaic) ', 
+                1:'The above leaf is Cassava CB (Cassava Bacterial Blight)', 
+                2:'The above leaf is Cassava Healthy leaf', 
+                3:'This is not trained yet',
+                4:'The above leaf is Tomato Bacterial spot', 
+                5:'The above leaf is Tomato early blight',
+                6:'The above leaf is Tomato Late blight',
+                7:'The above leaf is Tomato Leaf Mold', 
+                8:'The above leaf is Tomato Septoria leaf spot',
+                9:'The above leaf is Tomato Spider mites Two-spotted spider mite', 
+                10:'The above leaf is Tomato Target Spot',
+                11:'The above leaf is Tomato Yellow Leaf Curl Virus', 
+                12:'The above leaf is Tomato mosaic virus', 
+                13:' The above leaf is Tomato healthy', 
+                14:'The above leaf is bean angular leaf spot',
+                15:'The above leaf is bean healthy', 
+                16:'The above leaf is bean rust'})
 remedies = {
     'The above leaf is Cassava (Cassava Mosaic)': [
          'Remedy for Cassava Mosaic', 'കാസവ മോസായികയുടെ പരിഹാരം',
@@ -48,33 +47,30 @@ selected_language = 'English'  # Set the default language
 
 num_ftrs = model.fc.in_features
 model.fc = torch.nn.Linear(num_ftrs, len(classes))
-model_path = "epoch-90.pt"
+model_path = "epoch-8.pt"
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-summary(model, input_size=(3, 224, 224))
 model.eval()
 
 # Preprocessing
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Resize((224, 224)),
-    transforms.RandomAffine(degrees=(25)),
-    transforms.RandomRotation(25),
-    transforms.RandomHorizontalFlip(0.5),
-    transforms.RandomVerticalFlip(0.5),
-    transforms.Normalize((0.5, 0.5, 0.5), (1, 1, 1))
-])
+transform=transforms.Compose([
+transforms.ToTensor(),
+    transforms.Resize((224,224)),
+    transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1),
+    transforms.RandomAffine(degrees=40, translate=None, scale=(1, 2), shear=15),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+    ])
 
 def model_predict(image, model_func, transform):
     image_tensor = transform(image).float()
     image_tensor = image_tensor.unsqueeze(0)
+    image_tensor=image_tensor.to(device)
     output = model_func(image_tensor)
     index = torch.argmax(output)
     pred = classes[index.item()]
     probs, _ = torch.max(F.softmax(output, dim=1), 1)
-    if probs < 0.93:
-        return "not defined", probs
-    else:
-        return pred, probs
+    return pred, probs
 
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:

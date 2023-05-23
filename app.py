2ceb2fac1df8d2bb00fc89deefe306a248db8c12
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-23 21:58:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -221,15 +221,7 @@ def contact_page():
 
 
 
-# Initialize SessionState
-def init_session_state():
-    if 'session_state' not in st.session_state:
-        st.session_state.session_state = {
-            'pred': None,
-            'probs': None,
-            'selected_language': 'English',
-            'language_selected': False
-        }
+
 
 def main():
     init_session_state()
